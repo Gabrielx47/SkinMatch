96174d53e1745eaedcb92b84f06e386714bab967
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2025-03-14 14:52:35 -0300

    Definição da qualidade, formato e proporção da imagem.

--- app/webcam.tsx
+++ app/webcam.tsx
@@ -10,46 +10,49 @@ export default function WebcamView({setImageUri, setTone}: WebcamViewProps) {
     const navigation = useNavigation<any>();
     const camera = useRef<CameraView>(null);
 
-    const pickImageFromTheWebcam = async () => {
-        setTone(' ');
-        //ToDo: mudar o formato da imagem para .jpeg
-        const permissionResult = await Camera.getCameraPermissionsAsync();
+  const pickImageFromTheWebcam = async () => {
+    setTone(' ');
+    //ToDo: mudar o formato da imagem para .jpeg
+    const permissionResult = await Camera.getCameraPermissionsAsync();
 
-        if (!permissionResult.granted) {
-            alert("É necessário permissão para acessar a câmera!");
-            return;
-        }
-
-        const picture = await camera.current?.takePictureAsync();
-        
-        if (picture) {
-            console.log("Imagem capturada pela webcam: " + picture.uri);
-            return picture.uri;
-        }
+    if (!permissionResult.granted) {
+      alert("É necessário permissão para acessar a câmera!");
+      return;
     }
+
+    const picture = await camera.current?.takePictureAsync({
+      imageType: 'jpg',
+      quality: 0.5,
+    });
+        
+    if (picture) {
+      console.log("Imagem capturada pela webcam: " + picture.uri);
+      return picture.uri;
+    }
+  }
     
   const updateImage = async () => {
     setImageUri(await pickImageFromTheWebcam()); 
     navigation.goBack('index');
-  }
+   }
 
     return (
-        <CameraView style={styles.WebcamView} ref={camera} >
-            <TouchableOpacity onPress={() => {updateImage()} }>
-                <Ionicons name="camera" size={64} color={'#FDFEFE'}/>
-            </TouchableOpacity>
-        </CameraView>   
+      <CameraView style={styles.WebcamView} ref={camera} pictureSize='460x640'>
+        <TouchableOpacity onPress={() => {updateImage()} }>
+          <Ionicons name="camera" size={64} color={'#FDFEFE'}/>
+        </TouchableOpacity>
+       </CameraView>   
     )
 } 
 
 const styles = StyleSheet.create({
-    WebcamView: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'flex-end',
-        flexDirection: 'row',
-    },
-    takePhotoButton: {
+  WebcamView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    flexDirection: 'row',
+  },
+  takePhotoButton: {
     backgroundColor:  '#17181A'
   },
 });
